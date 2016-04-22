0f28c6364f4776c8bbcb4194ede31e64f1ba82cc
jonsedar <jon@sedar.co>
2016-04-22 17:12:36 +0100

    improved basic linear regression
    writing both pymc3 and pystan models and traces to disk

--- convenience_functions.py
+++ convenience_functions.py
@@ -1,9 +1,42 @@
 # Convenience functions
+# Requires python 3.5
 
+import os
+import pickle
 import string
 import regex as re
 import numpy as np
 import pandas as pd
+
+
+
+def write_pickle(obj, relnm):
+    """ Serialize object to pickle and write to disk at relnm """
+   
+    with open(relnm, 'wb') as f:
+        pickle.dump(obj, f, protocol=-1)
+    return 'Serialized object to disk at {}'.format(relnm)
+
+
+def read_pickle(relnm):
+    """ Read serialized object from pickle ondisk at relnm """
+   
+    with open(relnm, 'rb') as f:
+        obj = pickle.load(f)
+    return obj
+
+
+
+def ensure_dir(relnm):
+    """ Accept relative filepath string, create it if it doesnt already exist
+        return filepath string
+    """
+    
+    d = os.path.dirname(relnm)
+    if not os.path.exists(d):
+        os.makedirs(d)
+        
+    return relnm
 
 
 def snakey_lowercase(s):
@@ -18,7 +51,7 @@ def snakey_lowercase(s):
 
 
 def custom_describe(df, nrows=3, nfeats=20):
-    ''' Conv fn: concat transposed topN rows, numerical desc & dtypes '''
+    ''' Concat transposed topN rows, numerical desc & dtypes '''
 
     print(df.shape)
     rndidx = np.random.randint(0,len(df),nrows)
---
+++ models/README_MODELS.md
@@ -0,0 +1,7 @@
+##### Jonathan Sedar Personal Project
+
+# PyMC3 Vs PyStan Comparison
+_Spring 2016_
+
+Placeholder for models dir
+
